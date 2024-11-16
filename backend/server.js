f8d6716111874f993cfedc9cd28dc987df5f1105
R.Sathish kumar <115446419+Sathiskumarssf@users.noreply.github.com>
2024-11-17 01:32:45 +0530

    Update server.js
    the vertil link

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,14 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+
+app.use(cors(
+{
+origin: ["https://deploy-mern-lihq.vercel.app"],
+methods: ["POST", "GET"],
+credentials: true
+}
+));
 app.use(express.json());
 
 // MongoDB Connection
